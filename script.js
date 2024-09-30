514f321eff982a5d492c5e240c78c1dedd92fed2
notDevSec <notthededsec.dev@gmail.com>
2024-09-30 21:57:20 +0600

    2nd edition

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 'use strict';
 
+let table = document.querySelector('table');
+let data = localStorage.getItem('passwords');
+
+if (data == null) {
+  table.innerHTML = '<tr><td colspan="3">No data to show</td></tr>';
+} else {
+  let arr = JSON.parse(data);
+  let str = '';
+
+  for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+
+    str += `<tr>
+      <td>${element.website}</td>
+      <td>${element.username}</td>
+      <td>${element.password}</td>
+    </tr>`;
+  }
+
+  table.innerHTML += str; // Append rows to the table
+}
+
 document.querySelector('.btn').addEventListener('click', (e) => {
   e.preventDefault();
 
